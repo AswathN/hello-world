ca0b4157942070ea08f51be98014bc49730efd78
qaautomated <qaautomated@AEA-QA09.aeaonline.net>
2021-08-24 07:22:42 -0400

    New Files Added

---
+++ src/framework/CreateApp.java
@@ -0,0 +1,17 @@
+package framework;
+
+public class CreateApp {
+
+	public static void main(String[] args) throws Throwable {
+
+		try {
+			System.out.println("<<< Program Starts >>>");
+			System.out.println("Hello World!!!");
+			System.out.println("App is successfully created");
+			System.out.println("<<< Program Ends >>>");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
