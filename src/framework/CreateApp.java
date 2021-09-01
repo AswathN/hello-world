096fcd66b8e85daba16752027857a48229e82a75
AswathN <88755255+AswathN@users.noreply.github.com>
2021-09-01 08:41:37 -0400

    Update CreateApp.java

--- src/framework/CreateApp.java
+++ src/framework/CreateApp.java
@@ -7,7 +7,7 @@ public class CreateApp {
 		try {
 			System.out.println("<<< Program Starts >>>");
 			System.out.println("Hello World!!!");
-			System.out.println("Application is successfully created.");
+			System.out.println("Application is successfully created!");
 			System.out.println("<<< Program Ends >>>");
 		} catch (Exception e) {
 			e.printStackTrace();
